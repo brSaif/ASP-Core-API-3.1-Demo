f8db9614b725c8b340c26b75ca010e7a8b3630e2
Saibr <riahi1100@gmail.com>
2021-05-08 02:33:10 +0100

    Create an association Controller to get the Talks of a Camp By moniker

---
+++ ASP Core API 3.1 Demo/Controllers/TalksController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP_Core_API_3._1_Demo.Data;
+using ASP_Core_API_3._1_Demo.Data.Entities;
+using ASP_Core_API_3._1_Demo.Model;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace ASP_Core_API_3._1_Demo.Controllers
+{
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksController : Controller
+    {
+        private readonly ICampRepository _repository;
+        private readonly ILogger<TalksController> _logger;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository repository,
+            ILogger<TalksController> logger,
+            IMapper mapper,
+            LinkGenerator linkGenerator)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get(string moniker)
+        {
+            try
+            {
+                var talks = await _repository.GetTalksByMonikerAsync(moniker);
+                return Ok(_mapper.Map<IEnumerable<Talk>, IEnumerable<TalkModel>>(talks));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed To Load Data. The following exception is thrown {ex}");
+            }
+            return BadRequest("Failed To Get Camp");
+}
+    }
+}
